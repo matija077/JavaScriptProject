2b8056379b2c8f29dcd66c53b51f55a1aec0f4ca
Matija077 <matija.prs@gmail.com>
2020-12-02 18:18:11 +0100

    some basics

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+    * collapse main nav to 3 dots when scrollign
+    * and make it fixed. when hoovered or clicked
+    * open back the full menu. when out of focus
+    * return to 3 dots
+    *
+*/
