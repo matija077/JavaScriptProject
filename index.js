441dc90a6ef71c57a4de6dc8d34a4c319ee62755
Matija077 <matija.prs@gmail.com>
2020-12-22 16:16:24 +0100

    some impvomenets
    add pointerEvents instead of mouseEvents
    tryign to stop mosueEvents, still trying
    fix footer
    add margins between side menus and content

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ var leftMenu = document.getElementsByClassName("side-navigation")[0];
 var leftMenuContainer = document.getElementsByClassName("side-navigation-container")[0];
 var leftMenuHidder = document.getElementsByClassName("side-navigation-container__menu-hider")[0];
 
-leftMenuContainer.onmouseenter = function(event) {
+leftMenuContainer.onpointerenter = function(event) {
     leftMenu.classList.remove("hidden");
     leftMenuContainer.classList.remove("side-navigation-container--collapsed");
     leftMenuHidder.classList.add("hidden");
+    event.preventDefault();
+    event.stopPropagation();
 }
 
-leftMenuContainer.onmouseleave = function(event) {
+leftMenuContainer.onpointerleave = function(event) {
     leftMenuHidder.classList.remove("hidden");
     leftMenuContainer.classList.add("side-navigation-container--collapsed");
     leftMenu.classList.add("hidden");
+    event.preventDefault();
+    event.stopPropagation();
+}
+
+leftMenuContainer.onmouseenter = function(event) {
+    console.log(event);
 }
