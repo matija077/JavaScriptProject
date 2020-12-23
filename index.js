c7de43088b87d0af05fe4fe71608e44745ca7e5f
Matija077 <matija.prs@gmail.com>
2020-12-23 12:00:15 +0100

    make right menu collapse and expand

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var leftMenu = document.getElementsByClassName("side-navigation")[0];
-var leftMenuContainer = document.getElementsByClassName("side-navigation-container")[0];
+var leftMenu = document.getElementsByClassName("content__side_navigation")[0];
+var leftMenuContainer = document.getElementsByClassName("content__side-navigation-container")[0];
 var leftMenuHidder = document.getElementsByClassName("side-navigation-container__menu-hider")[0];
+var rightMenuContainer = document.getElementsByClassName("content__side-paragraph")[0];
 
 leftMenuContainer.onpointerenter = function(event) {
     leftMenu.classList.remove("hidden");
     leftMenuContainer.classList.remove("side-navigation-container--collapsed");
     leftMenuHidder.classList.add("hidden");
+    rightMenuContainer.classList.add("side-paragraph--shrink");
     event.preventDefault();
     event.stopPropagation();
 }
@@ -14,6 +16,7 @@ leftMenuContainer.onpointerleave = function(event) {
     leftMenuHidder.classList.remove("hidden");
     leftMenuContainer.classList.add("side-navigation-container--collapsed");
     leftMenu.classList.add("hidden");
+    rightMenuContainer.classList.remove("side-paragraph--shrink");
     event.preventDefault();
     event.stopPropagation();
 }
