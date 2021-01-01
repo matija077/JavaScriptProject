8e767b3f6d0f107055912329635669e16a8a20e0
Matija077 <matija.prs@gmail.com>
2021-01-01 18:40:26 +0100

    trying to work on some things

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ var leftMenu = document.getElementsByClassName("content__side_navigation")[0];
 var leftMenuContainer = document.getElementsByClassName("content__side-navigation-container")[0];
 var leftMenuHidder = document.getElementsByClassName("side-navigation-container__menu-hider")[0];
 var rightMenuContainer = document.getElementsByClassName("content__side-paragraph")[0];
+var mainContent = document.getElementsByClassName("content-container")[0];
+
+var currentPageObject = {
+    id: "home",
+    pages: {
+        home: {
+
+        },
+        about: {
+
+        },
+        gallery: {
+
+        }
+    },
+};
 
 leftMenuContainer.onpointerenter = function(event) {
     leftMenu.classList.remove("hidden");
@@ -31,5 +47,24 @@ rightMenuContainer.onpointerleave = function(event) {
 }
 
 function mainMenuClickHandler(event) {
-    // event.preventDefault();
+    event.preventDefault();
+
+    var newPageId = event.currentTarget.dataset.id.toLowerCase();
+
+    if (currentPageObject.id === newPageId) {
+        return;
+    }
+
+    fetch(`src/${newPageId}/${newPageId}.html`, {
+        headers: {
+            'Content-type': 'text/html'
+        }
+    }).
+    then(function resolved(result) {
+        var reader = result.body.getReader();
+        console.log(reader);
+    }).
+    catch(function rejected(error) {
+        console.log(error);
+    })
 }
